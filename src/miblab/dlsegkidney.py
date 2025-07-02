94c94ba9c86f1081c5c8c8aa7087348fd8e23d2d
Steven Sourbron <s.sourbron@sheffield.ac.uk>
2025-07-02 11:01:20 +0100

    updates dlseg

--- src/miblab/dlsegkidney.py
+++ src/miblab/dlsegkidney.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import tempfile
 
 import numpy as np
+import vreg
 
 from miblab.data import zenodo_fetch
 from miblab.data import clear_cache_datafiles
@@ -47,7 +49,7 @@ except ImportError:
     scipy_installed = False
 
 
-def kidney_pc_dixon(input_array, model='unetr', device=None, overlap=0.3, postproc=True, clear_cache = False, verbose=False):
+def kidney_pc_dixon_unetr(input_vol, device=None, overlap=0.3, postproc=True, clear_cache = False, verbose=False):
 
     """
     Segment individual kidneys on post-contrast Dixon images.
@@ -55,18 +57,11 @@ def kidney_pc_dixon(input_array, model='unetr', device=None, overlap=0.3, postpr
     This requires 4-channel input data with out-phase images, 
     in-phase images, water maps, and fat maps.
 
-
     Args:
-        input_array (numpy.ndarray): A 4D numpy array of shape 
+        input_vol (vreg.Volume3D): A 4D vreg volume of shape 
             [x, y, z, contrast] representing the input medical image 
             volume. The last index must contain out-phase, in-phase, 
             water and fat images, in that order.
-        model (str): 
-            model = 'unetr' uses a pretrained UNETR-based model in MONAI, hosted on 
-            `Zenodo <https://zenodo.org/records/15521814>`_
-            model = 'nnunet' uses a pretrained nnUNet-based model, hosted on 
-            `Zenodo <https://zenodo.org/records/15328218>`_
-            under the hood this runs nnUNetPredictor (for more details `MIC-DKFZ Wiki <https://deepwiki.com/MIC-DKFZ/nnUNet>`_)
         device (str):
             processor on which to deply the computation. If this is not 
             provided, this defaults to "cuda" (if this is available) and 
@@ -83,18 +78,174 @@ def kidney_pc_dixon(input_array, model='unetr', device=None, overlap=0.3, postpr
         verbose (bool): If True, prints logging messages.
 
     Returns:
-        dict: 
-            A dictionary with the keys 'leftkidney' and 
-            'rightkidney', each containing a binary NumPy array 
-            representing the respective kidney mask.
-    Example:
+        vreg.Volume3D: 
+            A volume with a kidneys as label array.
+    """
 
-        >>> import numpy as np
-        >>> import miblab
-        >>> data = np.random.rand(128, 128, 30, 4)
-        >>> mask = miblab.kidney_pc_dixon(data)
-        >>> print(mask['kidney_left'])
-        [0 1 1 ... 0 0 0]
+    if not torch_installed:
+        raise ImportError(
+            'torch is not installed. Please install it with "pip install torch".'
+            'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
+        )
+    if not monai_installed:
+        raise ImportError(
+            'monai is not installed. Please install it with "pip install monai".'
+            'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
+        )
+    if not scipy_installed:
+        raise ImportError(
+            'scipy is not installed. Please install it with "pip install scipy".'
+            'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
+        )
+
+
+    MODEL = 'UNETR_kidneys_v2.pth'
+    MODEL_DOI = "15521814"
+
+    input_array = input_vol.values
+
+    # Format input array
+    # ------------------
+    # TODO: make this consistent for future training
+    # swap the last two indices because UNETR is trained 
+    # with opposed-phase/in-phase/fat/water
+    input_array = input_array[..., [0, 1, 3, 2]]
+
+    # flip
+    if not input_vol.is_right_handed:
+        input_array = np.flip(input_array, axis=2)
+
+    # Rescale to required matrix size
+    old_shape = input_array.shape
+    new_shape = (320, 320, 144, 4)
+    zoom_factors = [n / o for n, o in zip(new_shape, old_shape)]
+    input_array = ndi.zoom(input_array, zoom_factors, order=1) 
+
+    # from (x,y,z,c) to (c,y,x,z)
+    input_array = np.transpose(input_array, (3, 0, 1, 2)) 
+
+    # Normalize data
+    input_array_out   = (input_array[0,...]-np.average(input_array[0,...]))/np.std(input_array[0,...])
+    input_array_in    = (input_array[1,...]-np.average(input_array[1,...]))/np.std(input_array[1,...])
+    input_array_water = (input_array[2,...]-np.average(input_array[2,...]))/np.std(input_array[2,...])
+    input_array_fat   = (input_array[3,...]-np.average(input_array[3,...]))/np.std(input_array[3,...])
+
+    input_array = np.stack((input_array_out, input_array_in, input_array_water, input_array_fat), axis=0)
+    # Convert to NCHW[D] format: (1,c,y,x,z)
+    # NCHW[D] stands for: batch N, channels C, height H, width W, depth D
+    input_array = input_array.transpose(0,2,1,3) # from (x,y,z) to (y,x,z)
+    input_array = np.expand_dims(input_array, axis=(0))
+    input_array = input_array.astype(np.float32)
+
+    if verbose:
+        print('(Down)loading model..')
+
+    weights_dir = importlib_resources.files('miblab.datafiles')
+    weights_path = zenodo_fetch(MODEL, weights_dir, MODEL_DOI)
+
+    if verbose:
+        print('Applying model to data..')
+
+    # Setup device
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    if device is None:
+        device_str = "cuda" if torch.cuda.is_available() else "cpu"
+    else:
+        device_str = device
+    device = torch.device(device_str)
+
+    # Define model architecture
+    model = UNETR(
+        in_channels=4,
+        out_channels=3, # BACKGROUND, RIGHT KIDNEY (left on image), LEFT KIDNEY (right on image)
+        img_size=(80, 80, 80),
+        feature_size=16,
+        hidden_size=768,
+        mlp_dim=3072,
+        num_heads=12,
+        proj_type="perceptron",
+        norm_name="instance",
+        res_block=True,
+        dropout_rate=0.0,
+    )
+    model.to(device)
+
+    # Convert to tensor
+    input_tensor = torch.tensor(input_array).to(device)
+
+    # Load model weights
+    weights = torch.load(weights_path, map_location=device, weights_only=True)
+    model.load_state_dict(weights)
+    model.eval() 
+
+    with torch.no_grad():
+        output_tensor = sliding_window_inference(input_tensor, (80,80,80), 4, model, overlap=overlap, device=device_str, progress=True) 
+
+    if verbose:
+        print('Post-processing results...')
+
+    # From probabilities for each channel to label image
+    output_tensor = torch.argmax(output_tensor, dim=1)
+
+    # Convert to numpy
+    output_array = output_tensor.numpy(force=True)[0,:,:,:]
+        
+    # Transpose to original shape
+    output_array = output_array.transpose(1,0,2) #from (y,x,z) to (x,y,z)
+
+    # Rescale to original matrix size
+    zoom_factors = [n / o for n, o in zip(old_shape[:3], new_shape[:3])]
+    output_array = ndi.zoom(output_array, zoom_factors, order=1) 
+
+    # flip back
+    if not input_vol.is_right_handed:
+        output_array = np.flip(output_array, axis=2)
+    
+    if postproc == True:
+        left_kidney, right_kidney = _kidney_masks(output_array)
+    else:
+        left_kidney=output_array[output_array == 2]
+        left_kidney[left_kidney==2]=1
+        right_kidney=output_array[output_array == 1]
+
+    kidneys = (left_kidney + 2*right_kidney).astype(np.int16)
+
+    if clear_cache:
+        if verbose:
+            print('Deleting downloaded files...')
+        clear_cache_datafiles(weights_dir)
+
+    return vreg.volume(kidneys, input_vol.affine) 
+
+
+def kidney_pc_dixon(input_array, device=None, postproc=True, clear_cache = False, verbose=False):
+
+    """
+    Segment individual kidneys on post-contrast Dixon images.
+
+    This requires 4-channel input data with out-phase images, 
+    in-phase images, water maps, and fat maps.
+
+    Args:
+        input_array (numpy.ndarray): A 4D numpy array of shape 
+            [x, y, z, contrast] representing the input medical image 
+            volume. The last index must contain out-phase, in-phase, 
+            water and fat images, in that order.
+        device (str):
+            processor on which to deply the computation. If this is not 
+            provided, this defaults to "cuda" (if this is available) and 
+            "cpu" otherwise.
+        postproc (bool): If True, applies post-processing to select 
+            the largest connected component from the UNETR output 
+            for each kidney mask
+        clear_cache: If True, the downloaded pth file is removed 
+            again after running the inference.
+        verbose (bool): If True, prints logging messages.
+
+    Returns:
+        numpy.ndarray: 
+            Label image
+
     """
 
     if not torch_installed:
@@ -107,146 +258,60 @@ def kidney_pc_dixon(input_array, model='unetr', device=None, overlap=0.3, postpr
             'totalsegmentator is not installed. Please install it with "pip install totalsegmentator".'
             'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
         )
-
-    if model == 'unetr':
-
-        MODEL = 'UNETR_kidneys_v2.pth'
-        MODEL_DOI = "15521814"
-
-        # Format input array
-        # ------------------
-        # TODO: make this consistent for future training
-        # swap the last two indices because UNETR is trained 
-        # with opposed-phase/in-phase/fat/water
-        input_array = input_array[..., [0, 1, 3, 2]]
-
-        # from (x,y,z,c) to (c,y,x,z)
-        input_array = np.transpose(input_array, (3, 0, 1, 2)) 
-
-        # Normalize data
-        input_array_out   = (input_array[0,...]-np.average(input_array[0,...]))/np.std(input_array[0,...])
-        input_array_in    = (input_array[1,...]-np.average(input_array[1,...]))/np.std(input_array[1,...])
-        input_array_water = (input_array[2,...]-np.average(input_array[2,...]))/np.std(input_array[2,...])
-        input_array_fat   = (input_array[3,...]-np.average(input_array[3,...]))/np.std(input_array[3,...])
-
-        input_array = np.stack((input_array_out, input_array_in, input_array_water, input_array_fat), axis=0)
-        # Convert to NCHW[D] format: (1,c,y,x,z)
-        # NCHW[D] stands for: batch N, channels C, height H, width W, depth D
-        input_array = input_array.transpose(0,2,1,3) # from (x,y,z) to (y,x,z)
-        input_array = np.expand_dims(input_array, axis=(0))
-        input_array = input_array.astype(np.float32)
-
-
-        if verbose:
-            print('(Down)loading model..')
-
-        temp_dir = importlib_resources.files('miblab.datafiles')
-        weights_path = zenodo_fetch(MODEL, temp_dir, MODEL_DOI)
-
-        if verbose:
-            print('Applying model to data..')
-
-        # Setup device
-        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        if device is None:
-            device_str = "cuda" if torch.cuda.is_available() else "cpu"
-        else:
-            device_str = device
-        device = torch.device(device_str)
-
-        # Define model architecture
-        model = UNETR(
-            in_channels=4,
-            out_channels=3, # BACKGROUND, RIGHT KIDNEY (left on image), LEFT KIDNEY (right on image)
-            img_size=(80, 80, 80),
-            feature_size=16,
-            hidden_size=768,
-            mlp_dim=3072,
-            num_heads=12,
-            proj_type="perceptron",
-            norm_name="instance",
-            res_block=True,
-            dropout_rate=0.0,
+    if not nib_installed:
+        raise ImportError(
+            'nibabel is not installed. Please install it with "pip install nibabel".'
+            'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
         )
-        model.to(device)
-
-        # Convert to tensor
-        input_tensor = torch.tensor(input_array).to(device)
-
-        # Load model weights
-        weights = torch.load(weights_path, map_location=device, weights_only=True)
-        model.load_state_dict(weights)
-        model.eval() 
-
-        with torch.no_grad():
-            output_tensor = sliding_window_inference(input_tensor, (80,80,80), 4, model, overlap=overlap, device=device_str, progress=True) 
-
-        if verbose:
-            print('Post-processing results...')
-
-        # From probabilities for each channel to label image
-        output_tensor = torch.argmax(output_tensor, dim=1)
-
-        # Convert to numpy
-        output_array = output_tensor.numpy(force=True)[0,:,:,:]
-            
-        # Transpose to original shape
-        output_array = output_array.transpose(1,0,2) #from (y,x,z) to (x,y,z)
-    
-    elif model == 'nnunet':
-        
-        MODEL = 'nnunet_kidneys_v2.zip'
-        MODEL_DOI = "15328218"
-
-        if not nib_installed:
-            raise ImportError(
-                'nibabel is not installed. Please install it with "pip install nibabel".'
-                'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
-            )
-        if not scipy_installed:
-            raise ImportError(
-                'scipy is not installed. Please install it with "pip install scipy".'
-                'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
-            )
-
-        if verbose:
-            print('(Down)loading model..')
-
-        temp_dir = importlib_resources.files('miblab.datafiles')
-        weights_path = zenodo_fetch(MODEL, temp_dir, MODEL_DOI, extract=True)
-
-        if verbose:
-            print('Applying model to data..')
-
-        # Setup device
-        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        if device is None:
-            device_str = "cuda" if torch.cuda.is_available() else "cpu"
-        else:
-            device_str = device
-        device = torch.device(device_str)
-
-        # Setup device
-        predictor = nnUNetPredictor(
-            tile_step_size=0.75,
-            use_gaussian=True,
-            use_mirroring=False,
-            perform_everything_on_device=True,
-            device=device,
-            verbose=False,
-            verbose_preprocessing=False,
-            allow_tqdm=True
+    if not scipy_installed:
+        raise ImportError(
+            'scipy is not installed. Please install it with "pip install scipy".'
+            'To install all dlseg options at once, install miblab as pip install miblab[dlseg].'
         )
+  
+    MODEL = 'nnunet_kidneys_v2.zip'
+    MODEL_DOI = "15328218"
 
-        # direct to "nnUNetTrainer__nnUNetPlans__3d_fullres"
-        nested_folder = os.path.join(weights_path, "nnUNetTrainer__nnUNetPlans__3d_fullres")
+    if verbose:
+        print('(Down)loading model..')
 
-        # Setup the predictor
-        predictor.initialize_from_trained_model_folder(
-            nested_folder,
-            use_folds='all',
-            checkpoint_name='checkpoint_best.pth'
-        )
+    weights_dir = importlib_resources.files('miblab.datafiles')
+    weights_path = zenodo_fetch(MODEL, weights_dir, MODEL_DOI, extract=True)
+
+    if verbose:
+        print('Applying model to data..')
+
+    # Setup device
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    if device is None:
+        device_str = "cuda" if torch.cuda.is_available() else "cpu"
+    else:
+        device_str = device
+    device = torch.device(device_str)
+
+    # Setup device
+    predictor = nnUNetPredictor(
+        tile_step_size=0.75,
+        use_gaussian=True,
+        use_mirroring=False,
+        perform_everything_on_device=True,
+        device=device,
+        verbose=False,
+        verbose_preprocessing=False,
+        allow_tqdm=True
+    )
+
+    # direct to "nnUNetTrainer__nnUNetPlans__3d_fullres"
+    nested_folder = os.path.join(weights_path, "nnUNetTrainer__nnUNetPlans__3d_fullres")
+
+    # Setup the predictor
+    predictor.initialize_from_trained_model_folder(
+        nested_folder,
+        use_folds='all',
+        checkpoint_name='checkpoint_best.pth'
+    )
+
+    with tempfile.TemporaryDirectory() as temp_dir:
 
         # Generate temp folder for intermediate data
         temp_folder_results = os.path.join(temp_dir,"temp_results")
@@ -274,22 +339,16 @@ def kidney_pc_dixon(input_array, model='unetr', device=None, overlap=0.3, postpr
 
         # Load the NIfTI file
         nifti_file = nib.load(os.path.join(temp_folder_results,'Dixon_999.nii.gz'))
-
         output_array = nifti_file.get_fdata()
     
     if postproc == True:
         left_kidney, right_kidney = _kidney_masks(output_array)
-
     else:
         left_kidney=output_array[output_array == 2]
         left_kidney[left_kidney==2]=1
-        
         right_kidney=output_array[output_array == 1]
 
-    kidneys = {
-        "kidney_left": left_kidney,
-        "kidney_right": right_kidney
-    }
+    kidneys = (left_kidney + 2*right_kidney).astype(np.int16)
 
     if clear_cache:
         if verbose:
